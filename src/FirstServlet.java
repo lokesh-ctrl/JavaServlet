9a0e0ad7e74574c979014216dd613eaf96a14de1
Rongali Lokesh <rlokesh@aconex.com>
2018-07-14 11:42:05 +0530

    creating first servlet which prints as hello from first servlet

---
+++ src/FirstServlet.java
@@ -0,0 +1,18 @@
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FirstServlet extends GenericServlet {
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        servletResponse.setContentType("text/html");
+        PrintWriter printWriter = servletResponse.getWriter();
+        printWriter.println("<B>Hello! This is from first servlet");
+        printWriter.close();
+
+    }
+}
